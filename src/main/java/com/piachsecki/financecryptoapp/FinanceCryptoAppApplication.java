e0c418ad26ae386b2d5f1272bae7e5b5c98c0ef7
Kacper Piasecki <k.r.piasecki2@gmail.com>
2024-05-19 15:59:52 +0200

    Setting up application

---
+++ src/main/java/com/piachsecki/financecryptoapp/FinanceCryptoAppApplication.java
@@ -0,0 +1,13 @@
+package com.piachsecki.financecryptoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCryptoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceCryptoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piachsecki/financecryptoapp/FinanceCryptoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piachsecki.financecryptoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceCryptoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
